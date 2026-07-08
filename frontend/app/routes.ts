51fa2a364d3e2a8c851c170a9799e5d611102bc7
astro-jets <mphatsomtogolo30@gmail.com>
2026-07-08 19:39:32 +0200

    seceond initial

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,29 +2,11 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   // Public Routes
-
-  route("/albums", "pages/public/albums.tsx"),
-  route("/market", "pages/public/market.tsx"),
-  route("/album/:id", "pages/public/album.tsx"),
-  route("/movies", "pages/movies/landing.tsx"),
-  index("pages/public/home.tsx"),
-
-  route("/maurice", "pages/maurice/landing.tsx"),
-  route("/maurice/about", "pages/maurice/about.tsx"),
-  route("/maurice/services", "pages/maurice/services.tsx"),
-  route("/maurice/insights", "pages/maurice/insights.tsx"),
-  route("/maurice/contact", "pages/maurice/contacts.tsx"),
-  route("/maurice/record", "pages/maurice/record.tsx"),
-
-  // Artist Routes
-  route("/artists", "pages/artist/dashboard.tsx"),
-  route("/artists/releases", "pages/artist/releases.tsx"),
-  route("/artists/releases/:id", "pages/artist/release.tsx"),
-  route("/artists/upload", "pages/artist/createrelease.tsx"),
-
-  // Admin Routes
-  route("/admin/dashboard", "pages/admin/dashboard.tsx"),
-  route("/admin/releases", "pages/admin/reviewrelease.tsx"),
-  route("/signup", "pages/auth/SignupPage.tsx"),
-  route("/login", "pages/auth/LoginPage.tsx"),
+  // index("pages/public/home.tsx"),
+  route("/", "pages/landing.tsx"),
+  route("/about", "pages/about.tsx"),
+  route("/services", "pages/services.tsx"),
+  route("/insights", "pages/insights.tsx"),
+  route("/contact", "pages/contacts.tsx"),
+  route("/record", "pages/record.tsx"),
 ] satisfies RouteConfig;
